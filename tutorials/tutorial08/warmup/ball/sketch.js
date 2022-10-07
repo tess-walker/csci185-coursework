97b3161865251392042a077b6ec248895f5fd237
Tess <hwalker2@unca.edu>
2022-10-07 12:44:57 -0400

    tutorial 8

---
+++ tutorials/tutorial08/warmup/ball/sketch.js
@@ -0,0 +1,27 @@
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight; 
+    
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+}
+
+let x = 200;
+let speed = 5;
+
+function draw() {
+    // frameRate(4);
+    clear();
+
+    // draw walls:
+    fill('red');
+    rect(100, 0, 50, canvasHeight);
+    rect(500, 0, 50, canvasHeight);
+
+    // draw circle:
+    fill('black');
+    circle(x, canvasHeight/2, 50);
+    x += speed;
+    console.log("x position is:", x);
+
+    drawGrid(canvasWidth, canvasHeight);
+}
