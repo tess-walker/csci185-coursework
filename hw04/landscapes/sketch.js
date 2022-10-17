ba9777623cb6341ce63a1774f56783fce234f605
Tess <hwalker2@unca.edu>
2022-10-17 18:38:56 -0400

    cars :)

---
+++ hw04/landscapes/sketch.js
@@ -0,0 +1,35 @@
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight; 
+
+async function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+    
+    // draws 4 stars: 
+    strokeWeight(0);
+    fill('white');
+
+    drawStars();
+}
+
+function drawStars() {
+    strokeWeight(0);
+    fill('white');
+    for (let i = 0; i < 1000; i++) {
+        const x = Math.random() * canvasWidth;
+        const y = Math.random() * canvasHeight;
+        const width = Math.random() * 2 + 0.5;
+        circle(x, y, width);
+    }
+}
+
+function drawBubbles() {
+    strokeWeight(1);
+    stroke('white');
+    noFill()
+    for (let i = 0; i < 500; i++) {
+        const x = Math.random() * canvasWidth;
+        const y = Math.random() * canvasHeight;
+        const width = Math.random() * 40 + 5;
+        circle(x, y, width);
+    }
+}
